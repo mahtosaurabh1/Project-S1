b0dfe5a19cd81a78f73576d9eb50a033b5d24474
saurabh <mahtosaurabh1@gmail.com>
2023-04-07 02:18:35 +0530

    component designed

--- mail-box/src/App.js
+++ mail-box/src/App.js
@@ -2,12 +2,13 @@ import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import './App.css';
 import EmailCompose from './components/EmailCompose/EmailCompose';
 import { Header } from './components/Header/Header';
-import Home from './components/Home/Home';
 import Inbox from './components/Inbox/Inbox';
 import Login from './components/Login/Login';
 import Sent from './components/Sent/Sent';
 import Signup from './components/Signup/Signup';
 import ProtectedRoute from './ProtectedRoute/ProtectedRoute';
+import Footer from './components/Footer/Footer';
+import Home from './components/Home/Home';
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
       <Route path='/inbox' element={<ProtectedRoute Component={Inbox}/>}/>
       <Route path='/sent' element={<ProtectedRoute Component={Sent}/>}/>
     </Routes>
+    <Footer/>
   </Router>
   );
 }
--- mail-box/src/components/Home/Home.js
+++ mail-box/src/components/Home/Home.js
@@ -1,8 +1,28 @@
 import React from 'react'
 import './home.css'
 function Home() {
+
+  let arr= [1,2,3,4,5];
   return (
-  <h1 className="heading">Welcome</h1>
+    <div className='parent-header'>
+      <div className='head'>
+        <h1>Welcome</h1>
+      </div>
+      <div className='list-item-container'>
+        {
+         arr.map((val,i)=>{
+          return(
+            <div key={i} className='list-item'>
+              <span>{i+10} July</span>
+              <span>DTE ENERGY MUSIC THEATRE</span>
+              <button className='btn'>Ticket</button>
+            <br />
+            </div>
+          )
+         })
+        }
+      </div>
+    </div>
   )
 }
 
